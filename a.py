e499750e41a496524b611b7a6aa2535d30c87cd6
atulit gaur <atulit23@gmail.com>
2024-08-22 17:26:26 +0530

    changes in classification module

--- a.py
+++ a.py
@@ -5,8 +5,8 @@ import json
 url = "http://127.0.0.1:8000/core/interference/" 
 
 data = {
-    "data": [62257, 'Chevrolet', 'Camaro 1SS', 2017, 50850, 'Gasoline', '455.0HP 6.2L 8 Cylinder Engine Gasoline Fuel', 'A/T', 'Orange', 'Black', 'None reported'],
-    "modelId": '210a8cf6-9132-483a-88be-3748fb1f3468',
+    "data": [2325.0, 15.0, 1.0, 0.0, 1.0, 8.489856653530703, 25.0, 1.0, 4.0, 1.0, 0.0, 0.0, 0.0, 1.283227506286419],
+    "modelId": '861baeff-9e9f-4f10-bd37-70d853fc52d2',
     "userId": '40',
 }
 
